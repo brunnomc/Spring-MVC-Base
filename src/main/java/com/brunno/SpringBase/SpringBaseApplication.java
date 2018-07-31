0436b50c38211f91cbb9d69860f21544fc1b4efe
Brunno Melo <brunnomc@gmail.com>
2018-07-31 16:47:33 -0300

    Creating project

---
+++ src/main/java/com/brunno/SpringBase/SpringBaseApplication.java
@@ -0,0 +1,12 @@
+package com.brunno.SpringBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brunno/SpringBase/SpringBaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brunno.SpringBase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
